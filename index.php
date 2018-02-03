fe00f9977abcf260a6b00e90e6a9005113b2dfcc
Philippe Xantus <pxantus@g.harvard.edu>
2018-02-02 22:20:53 -0500

    added image folder and image, added php index file with slight html

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Storm Trooper</h1>
+<p>Stormtroopers are elite shock troops fanatically loyal to the Empire and impossible to sway from the Imperial cause.
+ They wear imposing white armor, which offers a wide range of survival equipment and temperature controls to allow the 
+soldiers to survive in almost any environment.
+Stormtroopers wield blaster rifles and pistols with great skill, and attack in hordes to overwhelm their enemies. 
+Along with standard stormtroopers, the Empire has organized several specialized units, including snowtroopers and scout troopers.</p>
+<img src="/hello-world/image/stormtrooper.png" atl="stormtrooper" height="300" width="150">
+</body>
+</html>
